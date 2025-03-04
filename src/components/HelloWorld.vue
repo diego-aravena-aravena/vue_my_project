89e622f6d553ef2a65cfc8a12aaa521a87d3b16f
diego <diegojehovaaravena@gmail.com>
2025-03-04 14:42:43 -0300

    se add funciones

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,32 +6,52 @@ defineProps({
 })
 
 const count = ref(0)
+
+const texto = ref("cambio")
+
+const holaMundo = (params) => {
+  console.log(params);
+}
+
+function Cambiartexto(cambio) {
+  texto.value = cambio;
+}
+
+
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <h1>{{ texto }}</h1>
+
+  <!-- <div class="card">
+    <button type="button" @click="count++">pelotudo de ms es {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
+  </div> -->
+
+  <div>
+    <button type="button" @click="Cambiartexto('saludos')">saludos
+    </button>
+    <button type="button" @click="Cambiartexto('cordiales')">cordiales
+    </button>
+    <button type="button" @click="Cambiartexto('pepipo')">pepipo
+    </button>
+    <button type="button" @click="Cambiartexto('ct.....')">hola
+    </button>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
